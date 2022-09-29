674f41337982e8360f899b3de0f5f03234704a74
BorisTheLizard <99308714+BorisTheLizard@users.noreply.github.com>
2022-09-29 14:50:05 -0700

    bossLvl polishing

--- Assets/destroyerTrigg.cs
+++ Assets/destroyerTrigg.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class destroyerTrigg : MonoBehaviour
 {
+    [SerializeField] GameObject crashParticles;
+    AudioSource audioSource;
+    [SerializeField] AudioClip clip;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     public void OnTriggerEnter(Collider other)
     {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+        Instantiate(crashParticles, other.transform.position, Quaternion.identity);
         Destroy(other.gameObject);
     }
 }
