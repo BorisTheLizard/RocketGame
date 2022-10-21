e128712acd67de89f3a8f7b606219f74b5b89b2d
BorisTheLizard <99308714+BorisTheLizard@users.noreply.github.com>
2022-10-21 15:38:53 -0700

    Polishing/Cleaning

---
+++ Assets/pressSoundMaker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressSoundMaker : MonoBehaviour
+{
+    AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "triggerrator")
+        {
+            return;
+        }
+        else
+        {
+            Debug.Log("Collision sound play");
+            source.Play();
+        }
+    }
+}
